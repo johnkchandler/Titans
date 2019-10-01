bff6adfb5f1dca205bb05011a9ad6add29387090
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2019-10-01 18:35:07 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -21,10 +21,14 @@ function getClicked(element, character, success, failure) {
     header.innerHTML = "<h1>" + success + "</h1>";
     footer.innerHTML = "<a href='./index.html'>play again</a>";
     element.parentElement.classList.add("correct");
+	var correct_audio = new Audio('./audio/correct.mp3');
+    correct_audio.play();
   } else {
     element.classList.add("selected");
     element.parentElement.classList.add("selected");
     header.innerHTML = "<h1>" + failure + "</h1>";
+    var incorrect_audio = new Audio('./audio/incorrect.mp3');
+    incorrect_audio.play();
   }
 }
 
